ec61aa73e78c2bcf2d202e497d1663989bfd310a
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2020-12-22 23:21:03 +0100

    Create initial commit with client to simple graphql app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Inspiring quote</h1>
+      <Quote text="To be, or not to be..." author="Hamlet" />
+    </div>
+  );
+}
+
+function Quote({ text, author }) {
+  return (
+    <blockquote>
+      {text}
+      <footer>{author}</footer>
+    </blockquote>
+  );
+}

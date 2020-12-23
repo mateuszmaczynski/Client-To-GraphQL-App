5c5de3be7aabef874bbeec52d418d26cd96ddccb
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2020-12-23 23:26:41 +0100

    add apollo/client and component to display random quote

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { useQuery, gql } from "@apollo/client";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Inspiring quote</h1>
-      <Quote text="To be, or not to be..." author="Hamlet" />
+      <RandomQuote />
     </div>
   );
+}
+
+const RANDOM_QUOTE_QUERY = gql`
+  query getRandomQuote {
+    randomQuote {
+      text
+      author
+    }
+  }
+`;
+
+function RandomQuote() {
+  const { data, loading } = useQuery(RANDOM_QUOTE_QUERY);
+  if (loading) {
+    return "Quote is loading...";
+  }
+  const { text, author } = data.randomQuote;
+
+  return <Quote text={text} author={author} />;
 }
 
 function Quote({ text, author }) {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache
+} from "@apollo/client";
+// import reportWebVitals from './reportWebVitals';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+const URL = "https://examples.devmastery.pl/random-stuff/graphql";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: URL
+  })
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +32,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
